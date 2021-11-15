134e5ab3acef6a59d41d607246af6e58aa551f0f
Denys Yaroshenko <jaroshenkodenis97@gmail.com>
2021-11-15 19:30:40 +0200

    add stage option

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,13 @@ const { src, dest, series, parallel } = require('gulp')
 const flatMap = require('flat-map').default
 const path = require('path')
 const image = require('gulp-image')
+const newer = require('gulp-newer')
 const scaleImages = require('gulp-scale-images')
 const webp = require('gulp-webp')
 
 const options = {
-	mode: 'dev'	// 'dev' || 'prod'
+	mode: 'dev',	// 'dev' || 'stage' || 'prod'
+	tasks: 6
 }
 
 
@@ -33,31 +35,43 @@ const _size = {
 
 const _propsDev = {
 	pngquant: false, //lossy
-	optipng: ['-i 1', '-strip all', '-verbose', '-o7', '-force'], //lossless
+	optipng: ['-i 0', '-strip all', '-verbose', '-o1', '-force'], //lossless
 	zopflipng: false,  //lossless
+	jpegRecompress: false, //['--strip', '--quality', 'high', '--min', 70, '--max', 90],
+	mozjpeg: ['-optimize', '-progressive'],
+	gifsicle: ['--optimize=1'],
+	svgo: ['--enable', 'cleanupIDs', '--disable', 'convertColors'],
+	concurrent: options.tasks, //max parallels tasks
+	quiet: false, // defaults to false
+}
+
+const _propsStage = {
+	pngquant: false, //lossy
+	optipng: ['-i 0', '-strip all', '-verbose', '-force'], //lossless
+	zopflipng: false, //lossless
 	jpegRecompress: false, //['--strip', '--quality', 'high', '--min', 70, '--max', 90],
 	mozjpeg: ['-optimize', '-progressive'],
 	gifsicle: ['--optimize=3'],
 	svgo: ['--enable', 'cleanupIDs', '--disable', 'convertColors'],
-	concurrent: 10, //max parallels tasks
+	concurrent: options.tasks, //max parallels tasks
 	quiet: false, // defaults to false
 }
 
 const _propsProd = {
 	pngquant: false, //lossy
-	optipng: ['-i 1', '-strip all', '-verbose', '-o7', '-force'] , //lossless
-	zopflipng: ['-y', '-m', '--iterations=16', '--lossy_8bit', '--lossy_transparent'], //lossless
+	optipng: ['-i 0', '-strip all', '-verbose', '-force'], //lossless
+	zopflipng: ['-y', '-m', '--lossy_8bit', '--lossy_transparent'], //lossless
 	jpegRecompress: false, //['--strip', '--quality', 'high', '--min', 70, '--max', 90],
 	mozjpeg: ['-optimize', '-progressive'],
 	gifsicle: ['--optimize=3'],
 	svgo: ['--enable', 'cleanupIDs', '--disable', 'convertColors'],
-	concurrent: 24, //max parallels tasks
+	concurrent: options.tasks, //max parallels tasks
 	quiet: false, // defaults to false
 }
 
 const renderImage = async (srcPath = _path.src, pipeFunc, outPath = _path.out.main) => {
 	await new Promise((resolve) => {
-		src(srcPath).pipe(pipeFunc).pipe(dest(outPath)).on('end', resolve)
+		src(srcPath).pipe(newer(outPath)).pipe(pipeFunc).pipe(dest(outPath)).on('end', resolve)
 	})
 }
 
@@ -89,8 +103,14 @@ const toWebp = () => {
 	return renderImage(src, webp(), _path.out.webp)
 }
 
+const _getProps = (mode = options.mode) => {
+	if(mode === 'prod') return _propsProd
+	else if(mode === 'stage') return _propsStage
+	else return _propsDev
+}
+
 const optimize = (mode = options.mode) => {
-	const _props = mode === 'prod' ? _propsProd : _propsDev
+	const _props = _getProps(mode)
 	const src = `${_path.src}/**/*.${_path.ext}`
 	return renderImage(src, image(_props))
 }
@@ -98,6 +118,11 @@ const optimize = (mode = options.mode) => {
 const buildDev = (done) => {
 	options.mode = 'dev'
 	optimize('dev').then(() => done())
+}
+
+const buildStage = (done) => {
+	options.mode = 'stage'
+	optimize('stage').then(() => done())
 }
 
 const buildProd = (done) => {
@@ -110,4 +135,5 @@ exports.images = series(optimize)
 exports.resize = series(resizeImages)
 exports.build = parallel(optimize, toWebp)
 exports.buildDev = buildDev
+exports.buildStage = buildStage
 exports.buildProd = buildProd
